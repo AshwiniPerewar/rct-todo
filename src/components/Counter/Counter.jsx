e9a577abd7c6234ac8180952b10e5dd1f6fc28d7
AshwiniPerewar@gmail.com <ashwiniperewar411@gmail.com>
2022-05-24 01:47:04 +0530

    style

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 import styles from "./counter.module.css";
 
 const Counter = () => {
   // sample value to be replaced
-  let count = 0;
+  const[count,setCount]=useState(1);
   // NOTE: do not delete `data-cy` key value pair
   return (
     <div className={styles.counter}>
-      <button data-cy="task-counter-increment-button"></button>
+      <button data-cy="task-counter-increment-button" onClick={()=>{setCount(count+1)}} className={styles.plus}>+</button>
       <span data-cy="task-counter-value">{count}</span>
-      <button data-cy="task-counter-decrement-button"></button>
+      <button data-cy="task-counter-decrement-button" onClick={()=>{setCount(count-1)}} className={styles.plus}>-</button>
     </div>
   );
 };
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,16 +1,15 @@
 import React,{useState} from "react";
 import styles from "./task.module.css";
 
+import Counter from "../Counter/Counter";
 const Task = ({value,deleteTodo}) => {
   // NOTE: do not delete `data-cy` key value pair
 
-  const[count,setCount]=useState(1);
   return (
     <li data-cy="task" className={styles.task}>
       <input type="checkbox" data-cy="task-checkbox" className={styles.checkbox} />
-     {value}
+     {value}<Counter/>
      <div data-cy="task-text"></div>
-     <button onClick={()=>{setCount(count+1)}} className={styles.plus}>+</button>{count}<button onClick={()=>{setCount(count-1)}}>-</button>
       <button data-cy="task-remove-button" onClick={()=>{deleteTodo(value)}}> <span className="material-icons">delete</span></button>
     </li>
   );
